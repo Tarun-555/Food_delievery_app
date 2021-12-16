779f726f0b4a206da36d92d68d6c783af6b70997
Tarun <trn18r@gmail.com>
2021-12-16 23:04:22 +0530

    restaurant-screen-menuItem-card-update

--- constants.js
+++ constants.js
@@ -1,8 +1,13 @@
+import Veg from "./assets/vegan.png";
+import NonVeg from "./assets/non-vegan.png";
+
 const Primary = "#2a9d8f";
 const StatusbarColor = "#3FA69A";
-const Secondary = "#54B0A5"
+const Secondary = "#54B0A5";
 const greyBackground = "#ccc";
 const TextWhite = "#fff";
 const TextBlack = "#161616";
 
-export const Colors = { Primary,Secondary, StatusbarColor, greyBackground, TextWhite, TextBlack };
+export const Colors = { Primary, Secondary, StatusbarColor, greyBackground, TextWhite, TextBlack };
+
+export const veganType = { Veg, NonVeg };
